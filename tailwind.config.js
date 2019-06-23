75a28417a7e9999e5cda9851f97f368034e6a177
mzietz <zietzmaximilian@gmail.com>
2019-06-23 14:20:55 +0200

    added a lot

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+	    colors: {
+	      'ib-blue': '#247ba0',
+	      'ib-beige': '#f3ffbd',
+	      'ib-red': '#ff1654',
+	      'ib-lblue': '#70c1b3',
+	      'ib-llblue': '#b2dbbf',
+	    },
+    }
   },
   variants: {},
   plugins: []
