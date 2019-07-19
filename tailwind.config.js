3307c9eaf59f6599b412fb59b8a63f4b65e9dfb3
mzietz <zietzmaximilian@gmail.com>
2019-07-19 21:21:38 +0200

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	    colors: {
 	      'ib-blue': '#247ba0',
 	      'ib-beige': '#f3ffbd',
-	      'ib-red': '#ff1654',
+	      'ib-red': '#C20E25',
 	      'ib-lblue': '#70c1b3',
 	      'ib-llblue': '#b2dbbf',
 	      'ib-lllblue': '#dcefe2',
